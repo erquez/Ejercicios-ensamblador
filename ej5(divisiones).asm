108924cc9c3d08b7560b2b8ebaa20a6d0b820702
Erik Vázquez <erikk718@gmail.com>
2025-02-23 11:44:08 +0100

    Update ej5(divisiones).asm

--- ej5(divisiones).asm
+++ ej5(divisiones).asm
@@ -1,4 +1,4 @@
-TITLE DIVISIONES
+TITLE DIVISION
 
 Datos SEGMENT
 ; Declarar las variables aqui
@@ -28,42 +28,19 @@ MOV ES,AX
 ;Codigo abajo-->
 
 
+
+;Division de palabra:
  
- 
-;Suma
-MOV AL,5 ;despues de esto, AL tendra el valor 5
-ADD AL,7 ;despues de esto AL tendra el valor 12 (el valor max es 255 ya que AL tiene 8 bits)
-
-;Resta
-MOV AL,5 ;despues de esto, AL=5
-SUB AL,3 ;despues de esto, AL=2 
-
-
-
-
-;Multiplicacion
-;Resultado tipo Byte = Se almacena en AX (8 bits,255 valor max)
-MOV AL,255
-MOV BL,255
-MUL BL
-
-;Resultado tipo palabra = Se almacena en DX/AX (16 bits,65535 valor max)
-MOV AX,300
-MOV BX,500
-MUL BX
-;Resultado en DX =0002 y AX = 49F0 es decir, el resultado es 0249F0 = 150000 (concatenar DX con AX)
-
-
-
-
-;Division de palabra: 
 MOV AX,505
 MOV BL,3
 DIV BL
 ;Resultado(aparece en AL) = A8 , Residuo(aparece en AH) = 1 
 
 
+
+
 ;Division doble palabra entre palabra:
+
 ;El principio del numero mas grande se pone en DX y el final en AX
 MOV DX,002BH
 MOV AX,342AH
